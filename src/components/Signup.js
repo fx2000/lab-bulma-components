12f2b819c32c52bc46c212ad292bad695ae398ba
Daniel Duque <duque@outlook.com>
2019-11-04 23:45:07 +0100

    Iteration 3 finished

---
+++ src/components/Signup.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import CoolButton from "./CoolButton";
+import FormField from "./FormField";
+import Navbar from "./Navbar";
+
+export default function Signup() {
+  return (
+    <div>
+      <Navbar />
+      <form action="#" method="POST">
+        <FormField label="Name" type="text" placeholder="e.g. Alex Smith" />
+        <FormField label="Email" type="email" placeholder="e.g. alex@smith.com" />
+        <FormField label="Password" type="password" placeholder="********" />
+        <CoolButton isSuccess isRounded type="submit">Signup</CoolButton>
+      </form>
+    </div>
+  )
+}
